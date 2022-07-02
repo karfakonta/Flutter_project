96de9eb863e63239677b3948db6899fb990394c4
karfakonta <tontonkarfa@gmail.com>
2022-07-02 18:00:38 +0000

    starting point

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -12,58 +13,33 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'My First Flutter App',
-      home: Scaffold(
-        body: const MyStatefulWidget(),
+      title: 'Flutter Courses',
+      theme: ThemeData(
+        primaryColor: Colors.white70,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: const WelcomeScreen(),
+    );
+  }
+}
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: const Text('My Login Page'),
-          actions: <Widget>[
-            IconButton(icon: const Icon(Icons.add_alert), onPressed: (){
-                ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text('you press bell icon')));
-            },
-            ),
+          title: const Text("Login Page in Flutter"),
+        ),
+        body: Column(
+          children: <Widget> [
+            Image.asset("images/hotel1.jpg", width: 300, height: 300,),
+            Image.asset("images/hotel2.jpg", width: 300, height: 300,)
           ],
-        ),
-        ),
-      );
+        )
+    );
   }
 }
 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
 
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: <Widget> [
-          const SizedBox(height: 20),
-          Container(
-            height: 150,
-            width: 150,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.blue,
-              boxShadow: const[
-                BoxShadow(
-                  color: Colors.red,
-                  spreadRadius: 5,
-                  offset: Offset(0, 0)
-                ),
-              ],
-            ),
-            child: const Text('Container 2', style: TextStyle(fontSize: 25)),
-          )
-        ],
-      )
-    );
-}
-}
